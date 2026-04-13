aaf7d65543b686a91f7644f6a9d32256c365abd2
KingersKing <elliotwilliamcohen@gmail.com>
2026-04-12 23:07:30 -0500

    Load multiple scripts from GitHub in All.lua

---
+++ All.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/KingersKing/Test/refs/heads/main/Auto.lua'))()loadstring(game:HttpGet('https://raw.githubusercontent.com/KingersKing/Test/refs/heads/main/Egg.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/KingersKing/Test/refs/heads/main/LuckyBlock.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/KingersKing/Test/refs/heads/main/Sell.lua'))()
